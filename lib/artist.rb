cc39513106eee03a2e7274cbae3fd6a4c7eccc8d
Corey H <c.l.halicke@gmail.com>
2019-07-15 03:29:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,16 +14,18 @@ class Artist
   end
 
   def add_song(song)
+    @counter =+ 1
+
     @songs << song
     song.artist = self
-    @counter =+ 1
   end
 
   def add_song_by_name(song)
+    @counter =+ 1
+
     new_song = Song.new(song)
     new_song.artist = self
     @songs << new_song
-    @counter =+ 1
   end
 
   def self.song_count
