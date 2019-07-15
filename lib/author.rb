254970d7115ba19580a7cdc3f6262b0a4aeac4f7
Corey H <c.l.halicke@gmail.com>
2019-07-15 03:38:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,14 +14,14 @@ class Author
   def add_post(title)
     @posts << title
     title.author = self
-    @@counter =+ 1
+    @@counter += 1
   end
 
   def add_post_by_title(title)
     new_post = Post.new(title)
     new_post.author = self
     @posts << new_post
-    @@counter =+ 1
+    @@counter += 1
   end
 
   def self.post_count
