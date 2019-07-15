4dc88a399fb20126a4ea489a3cea03d6453f48ac
Corey H <c.l.halicke@gmail.com>
2019-07-15 03:33:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require "pry"
 class Artist
   attr_accessor :name
 
-  @counter = 0
+  @@counter = 0
 
   def initialize(name)
     @name = name
@@ -16,17 +16,17 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @counter =+ 1
+    @@counter =+ 1
   end
 
   def add_song_by_name(song)
     new_song = Song.new(song)
     new_song.artist = self
     @songs << new_song
-    @counter =+ 1
+    @@counter =+ 1
   end
 
   def self.song_count
-    @counter
+    @@counter
   end
 end
